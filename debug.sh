53e95428879b6c02c3ce907c293bfb22c30bb815
mwerlen <maxime@werlen.fr>
2018-08-07 16:43:05 +0200

    Add some example commands

---
+++ debug.sh
@@ -0,0 +1,1 @@
+./build/batchdetector --path="./samples_png" --ext="png" --debug --prefix="cible01_" --config="./config/cible_01.cfg"
---
+++ wait.sh
@@ -0,0 +1,1 @@
+./build/batchdetector --path="./samples_png" --ext="png" --wait --debug --prefix="cible01_" --config="./config/cible_01.cfg"
